de6129876d4a5f243de8b547bf6a04cd1c4750ab
hanxuguang <1102603375@qq.com>
2024-01-14 12:22:06 +0800

    初始化

---
+++ app/src/main/java/com/example/androidinsertflutter/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.androidinsertflutter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
+
+class MainActivity : FlutterActivity()
